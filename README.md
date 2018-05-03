ec26a89ba384e2200207813d568ff881749d561c
Cesar Celis <cesar.celis@2brains.cl>
2018-05-03 17:30:08 -0300

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# 2b_brainers
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: 2b_brainers
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
