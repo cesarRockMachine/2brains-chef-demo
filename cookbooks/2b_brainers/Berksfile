eaf4b92b986ef0f7bbb0636d781ca4003f45e5c1
Cesar Celis <cesar.celis@cumplo.com>
2018-08-30 12:43:13 -0300

    change cookbook name

--- cookbooks/2b_brainers/Berksfile
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-source 'https://supermarket.chef.io'
-metadata
-cookbook 'apache2-windows', '~> 1.0.0'
-cookbook 'apache2', '~> 5.0.1'
--- cookbooks/2b_brainers/attributes/serverstatus.rb
+++
@@ -1,5 +0,0 @@
-default['2b_brainers']['apache']['file_name'] = if node['platform'] == 'windows'
-                                                  ''
-                                                else
-                                                  '/etc/httpd/sites-enabled/hello.conf'
-                                                end
